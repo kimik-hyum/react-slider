5d46970bf490e119594f0c7c176f16555b85a532
kimik <familykim01@naver.com>
2019-07-19 16:30:53 +0900

    touch move 기능 추가

--- src/component/slider.js
+++ src/component/slider.js
@@ -7,22 +7,27 @@ export default class Slider extends React.Component {
     this.imageLength = this.props.data.length;
     this.size = 0;
     this.touchStartX = 0;
-    this.touchMoveX = 0;
+    this.moveX = 0;
+    this.liveData = props.data.slice();
     this.state = {
       index:props.startIndex,
       sliding:false,
-      active:true
+      active:true,
+      slideX:0
     }
   }
   
-  
+  componentWillMount() {
+    this.liveData.push(this.props.data[0]);
+    this.liveData.unshift(this.props.data[this.props.data.length-1]);
+  }
   componentDidMount() {
     this.getSlideSize();
     window.addEventListener("resize", this.getSlideSize);
   }
   getSlideSize = () => {
     this.size = this.slider.current.offsetWidth;
-    this.slideMove(this.state.index)
+    this.handleSlideMove(this.state.index)
   }
   prevNext = (num) => {
     const {index} = this.state;
@@ -30,19 +35,21 @@ export default class Slider extends React.Component {
     const idx = (index + num + imageLength) % imageLength;
     return idx;
   }
-  slideMove = (index) => {
+  handleSlideMove = (index) => {
     this.setState({
-      index
+      index,
+      slideX:index * this.size
     })
+
   }
   _renderBtn = () => ({
-    next:<button onClick={() => this.slideMove(this.prevNext(1))}>Next</button>,
-    prev:<button onClick={() => this.slideMove(this.prevNext(-1))}>Prev</button>
+    next:<button onClick={() => this.handleSlideMove(this.prevNext(1))}>Next</button>,
+    prev:<button onClick={() => this.handleSlideMove(this.prevNext(-1))}>Prev</button>
   })
   _renderNav = () => ({
     nav:<div className="slide-nav">
       {this.props.data.map((item,index) => {
-        return <button key={index} onClick={() => this.slideMove(index)}>{index+1}</button>
+        return <button key={index} onClick={() => this.handleSlideMove(index)}>{index+1}</button>
       })}
     </div>
   })
@@ -52,30 +59,34 @@ export default class Slider extends React.Component {
   }
   onSliderTouchStart = (e) => {
     const touches = e.touches[0];
-    this.touchStartX = touches.pageX
-   
+    this.touchStartX = touches.pageX;
   }
   onSliderTouchMove = (e) => {
     const touches = e.touches[0];
-    this.touchMoveX = this.touchStartX - touches.pageX;
+    this.moveX = this.touchStartX - touches.pageX
+    this.setState({
+      slideX:(this.touchStartX - touches.pageX) + (this.size * this.state.index)
+    })
   }
   onSliderTouchEnd = (e) => {
-    if(this.touchMoveX > 100){
-      this.slideMove(this.prevNext(1))
-    }else if(this.touchMoveX){
-      this.slideMove(this.prevNext(-1))
+    if(this.moveX > 100){
+      this.handleSlideMove(this.prevNext(1))
+    }else if(this.moveX < -100){
+      this.handleSlideMove(this.prevNext(-1))
+    }else{
+      this.handleSlideMove(this.state.index)
     }
   }
 
   render(){
-    const {data} = this.props;
+    const duration = this.props.duration/1000
     const {next,prev} = this._renderBtn()
     const {nav} = this._renderNav()
-    const duration = (this.props.duration / 1000) + "s"
-    const x = this.state.index * this.size;
     const sliderStyle = {
-      transition:"transform " + duration,
-      transform: `translate(${-x}px,0px)` 
+      transition:"transform " + duration+"s",
+      transform: `translate(${-this.state.slideX}px,0px)`,
+      position:'relative',
+      left:'-100%' 
     }
     return (
       <div className="slide">
@@ -83,7 +94,7 @@ export default class Slider extends React.Component {
         {next}
         <div className="slide-wrapper" style={sliderStyle} ref={this.slider} onTouchStart={this.onSliderTouchStart} onTouchMove={this.onSliderTouchMove} onTouchEnd={this.onSliderTouchEnd}>
           {
-            data.map((item,index) => {
+            this.liveData.map((item,index) => {
               return this._renderItem({item,index})
             })
           }
